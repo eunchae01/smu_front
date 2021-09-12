a13a76aed9faab0e5bf3e2a42e754430a9f93df5
tae2089 <tae2089002@gmail.com>
2021-09-12 15:01:02 +0900

    update

--- src/components/searchcomponents/Openedhied.js
+++ src/components/searchcomponents/Openedhied.js
@@ -1,27 +1,22 @@
-import {React,useState,useCallback} from 'react';
-import Openbutton from './opencomponenets/Openbutton';
-import ChampionRate from '../ChampionRate';
+import { React, useState, useCallback } from "react";
+import Openbutton from "./opencomponenets/Openbutton";
+import ChampionRate from "../championcomponents/ChampionRate";
 
-function Openedhied(){
+function Openedhied() {
+  const [clicked, setClicked] = useState(true);
 
-    const [clicked,setClicked] = useState(true)
+  const onClick = useCallback(() => {
+    console.log(clicked);
+    setClicked(() => !clicked);
+  }, [clicked]);
 
-    const onClick = useCallback(
-        () => {
-            console.log(clicked)
-            setClicked(()=> !clicked)
-        },
-        [clicked],
-    )
+  return (
+    <div>
+      <Openbutton onClick={onClick} clicked={clicked} />
 
-    return(
-        <div>
-            <Openbutton onClick= {onClick} clicked={clicked}/>
-
-            { clicked && <ChampionRate />  }
-
-        </div>
-    );
+      {clicked && <ChampionRate />}
+    </div>
+  );
 }
 
 export default Openedhied;
--- src/pages/Chamipon.js
+++ src/pages/Chamipon.js
@@ -1,78 +1,74 @@
-import React, { useState, useEffect, useCallback, createContext } from 'react';
-import './champion.css';
-import ChampionList from '../components/championcomponents/ChampionList';
-import axios from 'axios';
-import ChampionGrapList from '../components/Graphs/ChampionGrapList';
-import OutCome from '../components/complex/OutCome';
-import Test from '../components/searchcomponents/Test';
+import React, { useState, useEffect, useCallback, createContext } from "react";
+import "./champion.css";
+import ChampionList from "../components/championcomponents/ChampionList";
+import axios from "axios";
+import ChampionGrapList from "../components/Graphs/ChampionGrapList";
+import Test from "../components/searchcomponents/Test";
+import OutCome from "../components/complex/OutCome";
 
-export const Select = createContext('');
+export const Select = createContext("");
 function Chamipon() {
-    const [champions, setChampions] = useState([]);
-    const [championName, setchampionName] = useState('');
-    const [isClicked, setClicked] = useState(0);
-    const [imageUrl, setImageUrl] = useState('');
+  const [champions, setChampions] = useState([]);
+  const [championName, setchampionName] = useState("");
+  const [isClicked, setClicked] = useState(0);
+  const [imageUrl, setImageUrl] = useState("");
 
-    const onClick = (e) => {
-        setClicked(() => e.target.id);
-        setImageUrl(() => e.target.src);   
+  const onClick = (e) => {
+    setClicked(() => e.target.id);
+    setImageUrl(() => e.target.src);
+  };
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setchampionName(e.target.value);
+  };
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("/champion");
+        console.log(response.data);
+        setChampions(response.data); // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {
+        console.error(e);
+      }
     };
-    const onChange = (e) => {
-        console.log(e.target.value);
-        setchampionName(e.target.value);
-    };
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await axios.get('/champion');
-                console.log(response.data);
-                setChampions(response.data); // 데이터는 response.data 안에 들어있습니다.
-            } catch (e) {
-                console.error(e);
-            }
-        };
-        fetchUsers();
-    }, []);
+    fetchUsers();
+  }, []);
 
-    return (
+  return (
+    <div>
+      <div className="cpSearch">
+        <input
+          className="cpInput"
+          placeholder="챔피언 이름을 입력해주세요."
+          onChange={onChange}
+        />
+        <button type="submit" className="cpBtn">
+          검색하기
+        </button>
         <div>
-            <div className='cpSearch'>
-                <input
-                    className='cpInput'
-                    placeholder='챔피언 이름을 입력해주세요.'
-                    onChange={onChange}
-                />
-                <button type='submit' className='cpBtn'>
-                    검색하기
-                </button>
-                <div>
-                    {/* <Select.Provider value={isClicked}>
+          {/* <Select.Provider value={isClicked}>
                         <ChampionGrapList />
                     </Select.Provider> */}
-                </div>
-            </div>   
-
-            <div className='backPic'>
-                <div className='champBox'>
-                    <ChampionList
-                        chamipons={champions}
-                        championName={championName}
-                        onClick={onClick}
-                        className='championBox'
-                    />
-                </div>
-
-                <img className='champPic'  src = {imageUrl}/>
-                <div className='champRate'><Test id = {isClicked}/></div>
-
-                
-
-            </div>
-
-            
-                
         </div>
-    );
+      </div>
+
+      <div className="backPic">
+        <div className="champBox">
+          <ChampionList
+            chamipons={champions}
+            championName={championName}
+            onClick={onClick}
+            className="championBox"
+          />
+        </div>
+
+        <img className="champPic" src={imageUrl} />
+        <div className="champRate">
+          <Test id={isClicked} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chamipon;
